6fd0fc6fa87dd5f390848396118641f8f544fa4a
James Reynolds <115733317+James050988@users.noreply.github.com>
2023-01-12 10:48:12 +0000

    updated

---
+++ secret_Message.js
@@ -0,0 +1,22 @@
+/*
+Using array methods, you will transform an array of 
+strings into a secret message!
+
+You should consult the Mozilla Developer Network (MDN) for reference 
+on any method with which you are not familiar. */
+
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage.pop();
+
+secretMessage.push('to', 'program');
+
+secretMessage['easily'] = 'right';
+
+secretMessage.shift();
+
+secretMessage.unshift('Programming');
+
+secretMessage.splice(6, 11);
+
+console.log(secretMessage.join(' '));
